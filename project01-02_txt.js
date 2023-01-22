772c0c22018d48b8801db191885dc93fcb8d0180
preeve1 <91562189+preeve1@users.noreply.github.com>
2023-01-22 15:20:16 -0600

    Create project01-02_txt.js

---
+++ project01-02_txt.js
@@ -0,0 +1,16 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Patrick Reeves	
+    Date:   01/22/23
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+service1Name = “Basic”, service2Name = “Express”, service3Name = “Extreme”
+service4Name = “Ultimate”, service1Speed = “0 Mbps”, service2Speed = “100 Mbps”
+service3Speed = “500 Mbps”, and service4Speed = “1 Gig”
+
